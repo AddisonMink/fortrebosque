3c722f40e28ebe9216db62cc69ce794488793108
Addison Mink <addisonmink@Addisons-MacBook-Pro.local>
2026-05-03 17:02:26 -0500

    add bat.

---
+++ bat.lua
@@ -0,0 +1,42 @@
+function bat_behavior_new()
+    -- constants
+    local fly_anim = { frames = { 7, 8 }, fps = 6 }
+    local x_range = 40
+    local speed = 1
+
+    -- state
+    local target_y = nil
+
+    local state_map = {
+        hang = function(me, entities)
+            local player = entities[1]
+            local me_x, me_y = me.body.x, me.body.y
+            local p_x, p_y = player.body.x, player.body.y
+            local x_dist = p_x - me_x
+            if abs(x_dist) < x_range and p_y > me_y then
+                target_y = p_y - 4
+                me.anim = fly_anim
+                me.body.facing = p_x < me_x and -1 or 1
+                me.body.vel_x = me.body.facing * speed
+                return "fly"
+            end
+        end,
+        fly = function(me, entities)
+            if me.body.y < target_y then
+                me.body.vel_y += 0.2
+            else
+                me.body.vel_y = 0
+            end
+        end
+    }
+
+    return state_machine_new("hang", state_map)
+end
+
+bat = {
+    body = body_new(80, 40, 0, 0, 1, false),
+    hitbox = hitbox_new("enemy", 1),
+    hurtbox = hurtbox_new("player", 1, { x = 0, y = 0, w = 6, h = 6 }),
+    anim = { frames = { 9 }, fps = 1 },
+    update = bat_behavior_new()
+}
